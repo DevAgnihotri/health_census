e8a84508b17b479df5f8b2bc683de785d58f8c33
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-02 19:21:07 +0530

    added Search Condition

--- health_analysis.js
+++ health_analysis.js
@@ -25,6 +25,42 @@ function resetForm() {
     document.getElementById("condition").value = "";
 }
 
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv = '';
+
+    fetch('health_analysis.JSON')
+        .then(response => response.JSON)
+        .then(data => {
+            const condition = data.conditions.find(item => item.name.toLowerCase === input);
+
+            if(condition)
+            {
+                const symptoms = condition.symtoms.join(', ');
+                const prevention = condition.prevention.join(', ');
+                const treatment = condition.treatment;
+
+                resultDiv.innerHTML += `<h2>${condition}</h2>`;
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="condimg">`
+
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong>${symptoms}</p>`
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong>${prevention}</p>`
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong>${treatment}</p>`
+            }
+            else {
+                resultDiv.innerText = "Conditon Not Found.";
+            }
+        })
+        .catch(error => {
+            console.error("Error:",error);
+            resultDiv.innerText = "An error occured in fetching data.";
+        });
+}
+
+btnSearch.addEventListener('click',searchCondition);
+
+
 function generateReport() {
     const numPatients = patients.length;
     const conditionsCount = {
