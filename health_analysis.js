2caa69868a808d46c931ac885daea226fcb0f681
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-02 12:09:03 +0530

    added funtion generateReport

--- health_analysis.js
+++ health_analysis.js
@@ -24,3 +24,45 @@ function resetForm() {
     document.getElementById('input[name="gender"]:checked').value = false;
     document.getElementById("condition").value = "";
 }
+
+function generateReport() {
+    const numPatients = patients.length;
+    const conditionsCount = {
+        Diabetes: 0,
+        Thyroid: 0,
+        "High Blood Pressure": 0,
+    };
+    const genderConditionsCount = {
+        Male: {
+            Diabetes: 0,
+            Thyroid: 0,
+            "High Blood Pressure": 0,
+        },
+        Female: {
+            Diabetes: 0,
+            Thyroid: 0,
+            "High Blood Pressure": 0,
+        },
+    };
+
+    for(const patient of patients) {
+        conditionsCount[patient.condition]++;
+        genderConditionsCount[patient.gender][patient.condition]++;
+    }
+
+    report.innerHTML = `Number of patients: ${numPatients}<br><br>`;
+    report.innerHTML += `Condition Breakdown:<br>`;
+    for(const condition in conditionsCount) {
+        report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
+    }
+
+    report.innerHTML += `Gender Based Conditions`;
+    for(const gender in genderConditionsCount) {
+        report.innerHTML += `${gender}`;
+        for(const condition in genderConditionsCount) {
+            report.innerHTML += `&nbsp;&nbsp;${condition}: ${genderConditionsCount[condition]}<br>`;
+        }
+    }
+}
+
+addpatientbtn.addEventListener("click",addPatient);
